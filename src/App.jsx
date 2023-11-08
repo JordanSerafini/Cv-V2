7bc5d4ace732f03f37314fa6960ecce9bd7dc7ac
JordanSerafini <jordanserafini.74@gmail.com>
2023-11-08 19:03:04 +0100

    :construction: work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import HomeStart from './Components/Home-Start.jsx';
 import Navbar from './Components/Navbar.jsx';
@@ -11,11 +11,40 @@ import Cv from './Components/Cv.jsx';
 
 import './App.css';
 
+
+const useIntersectionObserver = (ref, callback, options = {}) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      callback(entry.isIntersecting);
+    }, { ...options, threshold: [0.25], rootMargin: "-100px 0px" });
+
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [ref, options, callback]);
+};
+
+
+
+
 function App() {
   const [showScroll, setShowScroll] = useState(false);
 
+  const competencesRef = useRef(null);
+  const skillRef = useRef(null);
+  const portfoglioRef = useRef(null);
+  const emailFormRef = useRef(null);
+  const cvRef = useRef(null);
+
   const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 400) { 
+    if (!showScroll && window.pageYOffset > 400) {
       setShowScroll(true);
     } else if (showScroll && window.pageYOffset <= 400) {
       setShowScroll(false);
@@ -25,9 +54,28 @@ function App() {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth', 
+      behavior: 'smooth',
     });
   };
+
+
+
+  // Utilisation de useIntersectionObserver pour chaque section
+  useIntersectionObserver(competencesRef, (isVisible) => {
+    if (isVisible) competencesRef.current.style.opacity = 1;
+  });
+  useIntersectionObserver(skillRef, (isVisible) => {
+    if (isVisible) skillRef.current.style.opacity = 1;
+  });
+  useIntersectionObserver(portfoglioRef, (isVisible) => {
+    if (isVisible) portfoglioRef.current.style.opacity = 1;
+  });
+  useIntersectionObserver(emailFormRef, (isVisible) => {
+    if (isVisible) emailFormRef.current.style.opacity = 1;
+  });
+  useIntersectionObserver(cvRef, (isVisible) => {
+    if (isVisible) cvRef.current.style.opacity = 1;
+  });
 
   useEffect(() => {
     window.addEventListener('scroll', checkScrollTop);
@@ -40,16 +88,24 @@ function App() {
     <>
       <HomeStart />
       <Navbar />
-      <Competences />
-      <Skill />
-      <Portfoglio />
-      <EmailForm />
+      <div ref={competencesRef} style={{ opacity: 0, transition: 'opacity 1s ease-out' }}>
+  <Competences />
+</div>
+<div ref={skillRef} style={{ opacity: 0, transition: 'opacity 1s ease-out' }}>
+  <Skill />
+</div>
+<div ref={portfoglioRef} style={{ opacity: 0, transition: 'opacity 1s ease-out' }}>
+  <Portfoglio />
+</div>
+<div ref={emailFormRef} style={{ opacity: 0, transition: 'opacity 1s ease-out' }}>
+  <EmailForm />
+</div>
 
-      <div className={`upBtn ${showScroll ? 'visible' : ''}`} >
-      <img onClick={scrollToTop} src={upLogo} alt="Retour en haut" />
-      < Cv showScroll={showScroll}/>
-    </div>
-      
+
+      <div className={`upBtn ${showScroll ? 'visible' : ''}`} onClick={scrollToTop}>
+        <img src={upLogo} alt="Retour en haut" />
+        <Cv />
+      </div>
     </>
   );
 }
