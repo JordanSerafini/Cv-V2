5cf7bff27de05aef7806434d114c04feec8e647d
JordanSerafini <jordanserafini.74@gmail.com>
2023-11-08 17:45:43 +0100

    :construction: work in progress

--- src/Components/Cv.jsx
+++ src/Components/Cv.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DlLogo from '../assets/dlLogo.png';
 import CvPDF from '/cv-alternance.pdf'; // Assurez-vous que le chemin d'accès au fichier est correct
 
 function Cv() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     function downloadCv() {
         const link = document.createElement("a");
         link.href = CvPDF;
         link.download = "cv-alternance.pdf";
-        document.body.appendChild(link); 
-        link.click(); 
-        document.body.removeChild(link); 
+        link.click();
     }
-    
+
+    function handleDownloadClick() {
+        setIsModalOpen(true);
+    }
+
+    function closeModal() {
+        setIsModalOpen(false);
+    }
+
+    function confirmDownload() {
+        downloadCv();
+        closeModal();
+    }
+
+    function handleOverlayClick(e) {
+        // Vérifie si le clic est sur l'overlay et non sur la modal elle-même
+        if (e.target.className === "Modal") {
+            closeModal();
+        }
+    }
 
     return (
-        <div className='Download-CV' onClick={downloadCv}>
+        <>
+            <div className='Download-CV' onClick={handleDownloadClick}>
+                <img src={DlLogo} alt="Download CV" />
+                <p>CV</p>
+            </div>
             
-            <img src={DlLogo} alt="Download CV" />
-            <p>CV</p>
-        </div>
+            {isModalOpen && (
+                <div className="Modal" onClick={handleOverlayClick}>
+                    <div className="Modal-content" onClick={e => e.stopPropagation()}>
+                        <p>Voulez-vous télécharger le CV ?</p>
+                        <div className="Modal-Btn-Container">
+                        <button className='Modal-Btn' onClick={confirmDownload}>Confirmer</button>
+                        <button className='Modal-Btn' onClick={closeModal}>Annuler</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
