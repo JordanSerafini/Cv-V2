8bc3c03ad4f93a291b014151e8bdc1e56be90004
JordanSerafini <jordanserafini.74@gmail.com>
2023-11-08 18:42:59 +0100

    :construction: work in progress

--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const EmailForm = () => {
   const [email, setEmail] = useState({
@@ -6,7 +6,7 @@ const EmailForm = () => {
     subject: '',
     text: ''
   });
-  
+
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
 
@@ -24,13 +24,16 @@ const EmailForm = () => {
         }
       });
 
-      const responseBody = await response.json();
-
-      if (!response.ok) {
-        throw new Error(responseBody.error || 'Failed to send email.');
+      // Ajout de vérification pour voir si le body de la réponse n'est pas vide
+      if (response.ok) {
+        const responseBody = await response.json();
+        setMessage(responseBody.message || 'Email sent successfully.');
+        setEmail({ email: '', subject: '', text: '' }); // Reset l'état de l'email
+      } else {
+        // Gestion des réponses non "ok"
+        const errorBody = await response.json();
+        throw new Error(errorBody.error || 'Failed to send email.');
       }
-
-      setMessage(responseBody.message);
     } catch (error) {
       console.error('Error sending email:', error);
       setError(error.message || 'Failed to send email. Please try again later.');
@@ -40,48 +43,48 @@ const EmailForm = () => {
   return (
     <div className='Contact'>
       <div className='Contact-Title-Container'>
-      <div className="Contact-Title">
-        Me contacter
-      </div>
-      <div className='Contact-Title-Background'>
-        Plus de renseignements ?
-      </div>
+        <div className="Contact-Title">
+          Me contacter
+        </div>
+        <div className='Contact-Title-Background'>
+          Plus de renseignements ?
+        </div>
       </div>
       <div className="Contact-Container">
- 
-      <form onSubmit={sendEmail} className='Form-Container'>
-        <div className="Contact-Entete">
-        <label className='Contact-Email-Container'>
-          Votre email:
-          <input  className='Contact-Email'
-            type="text" 
-            value={email.email} 
-            onChange={(e) => setEmail({ ...email, email: e.target.value })}
-          />
-        </label>
-
-
-        <label>
-          Sujet:
-          <input 
-            type="text" 
-            value={email.subject} 
-            onChange={(e) => setEmail({ ...email, subject: e.target.value })}
-          />
-        </label>
-        </div>
-
-        <label>
-          Message:
-          <textarea
-            value={email.text}
-            onChange={(e) => setEmail({ ...email, text: e.target.value })}
-          />
-        </label>
-        <button type="submit">Envoyer</button>
-      </form>
-      
-    </div>
+        <form onSubmit={sendEmail} className='Form-Container'>
+          <div className="Contact-Entete">
+            <label className='Contact-Email-Container'>
+              Votre email:
+              <input className='Contact-Email'
+                type="email" // Changement du type en email pour la validation native
+                value={email.email} 
+                onChange={(e) => setEmail({ ...email, email: e.target.value })}
+                required // Ajout de l'attribut required pour la validation
+              />
+            </label>
+            <label>
+              Sujet:
+              <input 
+                type="text" 
+                value={email.subject} 
+                onChange={(e) => setEmail({ ...email, subject: e.target.value })}
+                required // Ajout de l'attribut required pour la validation
+              />
+            </label>
+          </div>
+          <label>
+            Message:
+            <textarea
+              value={email.text}
+              onChange={(e) => setEmail({ ...email, text: e.target.value })}
+              required // Ajout de l'attribut required pour la validation
+            />
+          </label>
+          <button type="submit">Envoyer</button>
+        </form>
+        {message && <p className="success-message">{message}</p>}
+        {error && <p className="error-message">{error}</p>}
+      </div>
     </div>
   );
 };
